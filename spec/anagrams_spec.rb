34761746ff14a0c7183ce5a20ca88dcf2e30b986
Daniel Loyd <dhloyd@icloud.com>
2018-09-20 06:09:41 +0000

    automatically backed up by learn

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,13 +1,24 @@
-class Anagram
-
-  attr_accessor :name
-
-  def initialize(word)
-    @name = word
+describe 'Anagram' do
+  it 'should detect no matches' do
+    diaper = Anagram.new('diaper')
+    expect(diaper.match(%w(hello world zombies pants dipper))).to eq([])
   end
 
-  def match(array)
-    array.select {|x| x.split("").sort == @name.split("").sort}
+  it 'should detect a simple anagram' do
+    detector = Anagram.new('ba')
+    ba = detector.match(['ab', 'abc', 'bac'])
+    expect(ba).to eq(['ab'])
   end
 
-end 
+  it 'should detect an anagram' do
+    detector = Anagram.new('listen')
+    listen = detector.match %w(enlists google inlets banana)
+    expect(listen).to eq(['inlets'])
+  end
+
+  it 'should detect multiple anagrams' do
+    detector = Anagram.new('allergy')
+    allergy = detector.match %w(gallery ballerina regally clergy largely leading)
+    expect(allergy).to eq(['gallery', 'regally', 'largely'])
+  end
+end
